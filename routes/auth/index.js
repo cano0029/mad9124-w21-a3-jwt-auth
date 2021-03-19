4918ad2cc2d61fd3f03b5b7ef7ca00d6db676bdc
Johanna Cano <cano0029@algonquinlive.com>
2021-03-19 19:49:38 -0400

    authenticate user login

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -36,10 +36,45 @@ router.post('/users', sanitizeBody, async (req, res) => {
           status: '500',
           title: 'Server Error',
           description: 'Problem saving document to the database'
-        }
+        },
       ]
     })
   }
+})
+
+// user login and return an authentication token
+router.post('/tokens', sanitizeBody, async (req, res) => {
+  const { email, password } = req.sanitizedBody
+  const user = await User.findOne({ email: email }) // is the username valid? will return either: user object or null,
+  if (!user) {
+    return res.status(400).send({ 
+      errors: [
+        'Will come back to this'
+      ]
+    })
+  }
+
+  // if the supplied username is valid (it exists), we will now see if their password is also valid
+  // compare our database password (hashed password) for that user, with the password supplied by the user (payload.password)
+  const badHash = `$2b$${saltRounds}$invalidusernameaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa`
+  const hashedPassword = user ? user.password : badHash // if we have a user, use the user password. If the user does not match (return null), we will return bad hash (just to protect against hacks)
+
+  const passwordDidMatch = await bcrypt.compare(password, hashedPassword)
+  if (!passwordDidMatch) {
+    return res.status(400).send({ 
+      errors: [
+        {
+          status: '400',
+          title: 'Validation Error',
+          description: 'Come back to this later',
+        },
+      ]
+    })
+  }
+
+  // if email and password are both valid, return a token
+  const token = 'iamatoken'
+  res.status(201).send({ data: {token} })
 })
 
 export default router
