c4544ec76dea31cf1d71278cc8a85e403e9764b6
Johanna Cano <cano0029@algonquinlive.com>
2021-03-19 19:30:40 -0400

    Update POST route for User

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -10,6 +10,24 @@ router.post('/users', sanitizeBody, async (req, res) => {
   try {
     const newUser = new User(req.sanitizedBody)
     newUser.password = await bcrypt.hash(newUser.password, saltRounds)
+    
+    const userAlreadyExists = Boolean(
+      await User.countDocuments({ email : newUser.email })
+    )
+
+    if (userAlreadyExists) {
+      return res.status(400).send({
+        errors: [
+          {
+            status: '400',
+            title: 'Validation Error',
+            description: `Email address ${newUser.email} is already registered`,
+            source: { pointer: '/data/attributes/email'}
+          }
+        ]
+      })
+    }
+
     await newUser.save()
     res.status(201).send({ data: newUser })
   } catch (err) {
