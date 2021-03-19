332d7e8a3c7ab4ec7747aa89db578df484e78b65
Johanna Cano <cano0029@algonquinlive.com>
2021-03-19 19:21:56 -0400

    Add user password encryption

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,11 +1,15 @@
+import bcrypt from 'bcrypt'
 import express from 'express'
 import User from '../../models/User.js'
 import sanitizeBody from '../../middleware/sanitizeBody.js'
 const router = express.Router()
 
+const saltRounds = 14
+
 router.post('/users', sanitizeBody, async (req, res) => {
   try {
     const newUser = new User(req.sanitizedBody)
+    newUser.password = await bcrypt.hash(newUser.password, saltRounds)
     await newUser.save()
     res.status(201).send({ data: newUser })
   } catch (err) {
