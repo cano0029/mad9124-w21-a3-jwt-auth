1f0c665d5d65da1a779a159a1586fd5bdf3f99ff
Johanna Cano <cano0029@algonquinlive.com>
2021-03-19 19:13:57 -0400

    Add POST route for User

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,8 +1,24 @@
 import express from 'express'
+import User from '../../models/User.js'
+import sanitizeBody from '../../middleware/sanitizeBody.js'
 const router = express.Router()
 
-router.post('/users', async (req, res) => {
-  res.status(201).send({ data: 'new user created' })
+router.post('/users', sanitizeBody, async (req, res) => {
+  try {
+    const newUser = new User(req.sanitizedBody)
+    await newUser.save()
+    res.status(201).send({ data: newUser })
+  } catch (err) {
+    res.status(500).send({
+      errors: [
+        {
+          status: '500',
+          title: 'Server Error',
+          description: 'Problem saving document to the database'
+        }
+      ]
+    })
+  }
 })
 
 export default router
