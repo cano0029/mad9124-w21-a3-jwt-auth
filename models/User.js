7ea355cac8d9a86fe016aed3ae0c2a2bd46b043a
Johanna Cano <cano0029@algonquinlive.com>
2021-03-19 20:24:54 -0400

    Edit comments

--- models/User.js
+++ models/User.js
@@ -14,21 +14,18 @@ const schema = new mongoose.Schema ({
 })
 
 // if email and password are both valid, return a token
-schema.methods.generateAuthToken = function () { // do not use arrow function, need to use 'this'
+schema.methods.generateAuthToken = function () { 
   const payload = { uid: this._id }
   return jwt.sign(payload, jwtSecretKey)
 }
 
 // authenticate login info (email and password)
 schema.statics.authenticate = async function (email, password) {
-  // is the username valid based on email? will return either: user object or null,
-  const user = await this.findOne({ email: email }) 
-
-  // if the supplied username is valid (it exists), we will now see if their password is also valid
+  const user = await this.findOne({ email: email }) // is the username valid based on email? will return either: user object or null,
   const badHash = `$2b$${saltRounds}$invalidusernameaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa`
   const hashedPassword = user ? user.password : badHash // if we have a user, use the user password. If the user does not match (null), we will return bad hash (just to protect against hacks)
   
-  // compare our database password (hashed password) for that user, with the password supplied by the user (payload.password)
+  // Is supplied password also valid? Compare our database password (hashed password) for that user, with the password supplied by the user (payload.password)
   const passwordDidMatch = await bcrypt.compare(password, hashedPassword)
   
   return passwordDidMatch ? user : null // if password matched, return user. If it did not match, return null
